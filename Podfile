ab98fe758f493c618a16c87d79a88a3fddd9acc2
k.haigo <kojihaigo@gmail.com>
2019-10-05 09:32:28 +0900

    initial commit

---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SwiftUI-sample' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  pod 'R.swift', '~>5.0.0'
+
+  # Pods for SwiftUI-sample
+
+  target 'SwiftUI-sampleTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'SwiftUI-sampleUITests' do
+    # Pods for testing
+  end
+
+end
---
+++ SwiftUI-sampleTests/SwiftUI_sampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftUI_sampleTests.swift
+//  SwiftUI-sampleTests
+//
+//  Created by haigo koji on 2019/09/28.
+//  Copyright © 2019 haigo koji. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftUI_sample
+
+class SwiftUI_sampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
